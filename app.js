c55642a64c7d133acbf827352adcb3e3cc308594
Brian Etaghene <joveee05@gmail.com>
2022-11-01 02:54:50 +0100

    Updated app.js

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const globalErrorHandler = require('./controllers/error');
 const path = require('path');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 const AppError = require('./utils/appError');
 const userRouter = require('./routes/userRoutes');
 const postRouter = require('./routes/postRoutes');
@@ -14,9 +19,19 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(helmet());
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests. Try again in an hour',
+});
+
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
@@ -25,8 +40,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cookieParser());
 
+app.use(mongoSanitize());
+
+app.use(xss());
+
 app.use('/api/v1/users/', userRouter);
 app.use('/api/v1/posts/', postRouter);
+
+app.use(compression());
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
 app.use(globalErrorHandler);
 
